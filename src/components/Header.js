2a2d760b59caf958fe3d5b6d952a360b5d432bb9
jfaro <jfaro@umich.edu>
2020-09-08 14:41:02 -0400

    moved contact bar back to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,43 @@
 import React from "react";
 import "./Header.css";
 
+import IconButton from "@material-ui/core/IconButton";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+
 function Header() {
   return (
 
     <header>
       <h1>Julien Faro</h1>
+
+      <div className="icon-button-links">
+        <IconButton
+          href="https://www.instagram.com/julienfaro/"
+          target="_blank"
+          aria-label="instagram"
+        >
+          <InstagramIcon style={{ color: '#000000' }} fontSize="large" />
+        </IconButton>
+
+        <IconButton
+          href="https://www.linkedin.com/in/julien-faro-b5b856177/"
+          target="_blank"
+          aria-label="linkedin"
+        >
+          <LinkedInIcon style={{ color: '#000000' }} fontSize="large" />
+        </IconButton>
+
+        <IconButton
+          href="https://github.com/jfaro"
+          target="_blank"
+          aria-label="github"
+        >
+          <GitHubIcon style={{ color: '#000000' }} fontSize="large" />
+        </IconButton>
+      </div>
     </header>
   );
 }
