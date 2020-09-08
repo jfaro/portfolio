e94fd804307eeb22700fc778b422e279b5b8db0c
jfaro <jfaro@umich.edu>
2020-09-08 13:09:03 -0400

    simplified header by moving links to footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Header.css";
-import HeaderRight from "./HeaderRight";
-import HeaderLeft from "./HeaderLeft";
 
 function Header() {
   return (
+
     <header>
-      <HeaderLeft />
-      <HeaderRight />
+      <h1>Julien Faro</h1>
+
     </header>
   );
 }
